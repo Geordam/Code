a18f362ea55d73f1d8f97e68b7b5a11457fb44b8
Geordam <geordam.ly@gmail.com>
2016-12-31 16:51:49 +0000

    Update CodeInstall.sh

--- CodeInstall.sh
+++ CodeInstall.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+echo ======================== Enter API Keys =======================
 echo -n "Enter your Hipchat API key Send Notification : "
 read apikeymessage
 if [ -z "${apikeymessage}" ]; then
@@ -12,8 +13,14 @@ if [ -z "${apikeycreateroom}" ]; then
   echo '!!!!!! ERROR :   API key can not be empty. Exiting. Please retry   !!!!!!'
   exit 1
 fi
+echo -n "Enter your BitLy API key : "
+read apikeybitly
+if [ -z "${apikeybitly}" ]; then
+  echo '!!!!!! ERROR :   API key can not be empty. Exiting. Please retry   !!!!!!'
+  exit 1
+fi
 
-echo ======================== Update and upgrade=======================
+echo ======================== Update and upgrade =======================
 sudo apt-get update && sudo apt-get upgrade -y
 
 echo ======================== Install dependency =======================
@@ -46,3 +53,5 @@ drive init
 echo ======================== Adding API keys in the script file =======================
 sed -i 's/AUTH_TOKEN_NOTIFICATION=/AUTH_TOKEN_NOTIFICATION='$apikeymessage'/g' Code/ScriptCode.sh
 sed -i 's/AUTH_TOKEN_CREATEROOM=/AUTH_TOKEN_CREATEROOM='$apikeycreateroom'/g' Code/ScriptCode.sh
+sed -i 's/AUTH_TOKEN_BITLY=/AUTH_TOKEN_BITLY='$apikeybitly'/g' Code/ScriptCode.sh
+
