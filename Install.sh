5e6f4ceb4f1de0561ecfa8b2dccdb11269a5a985
Geordam <geordam.ly@gmail.com>
2016-12-31 18:21:51 +0000

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -5,12 +5,12 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 echo ======================== Install dependency =======================
 sudo apt-get install golang git mercurial -y
-sudo apt-get install build-essential software-properties-common -y
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update
-sudo apt-get install gcc-snapshot -y
-sudo apt-get update
-sudo apt-get install gcc-6 g++-6 -y
+#sudo apt-get install build-essential software-properties-common -y
+#sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+#sudo apt-get update
+#sudo apt-get install gcc-snapshot -y
+#sudo apt-get update
+#sudo apt-get install gcc-6 g++-6 -y
 
 echo ======================== Install go =======================
 curl -O https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz && sudo tar -C /usr/local -xzf go1.7.4.linux-amd64.tar.gz
