c926027280e4b0274c691bc9288320076fabea7a
Geordam <geordam.ly@gmail.com>
2016-12-31 14:08:58 +0000

    Update CodeInstall.sh

--- CodeInstall.sh
+++ CodeInstall.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-echo “======================== update and upgrade=======================”
+echo ======================== Update and upgrade=======================
 sudo apt-get update && sudo apt-get upgrade -y
 
-echo “======================== install dependency =======================”
+echo ======================== Install dependency =======================
 sudo apt-get install golang git mercurial -y
 sudo apt-get install build-essential software-properties-common -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
@@ -11,19 +11,20 @@ sudo apt-get install gcc-snapshot -y
 sudo apt-get update
 sudo apt-get install gcc-6 g++-6 -y
 
-echo “======================== install go =======================”
+echo ======================== Install go =======================
 curl -O https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz && sudo tar -C /usr/local -xzf go1.7.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
-echo “======================== export Variables =======================”
+echo “======================== Export Variables =======================
 cat << ! >> ~/.bashrc
 export GOPATH=\$HOME/gopath
 export PATH=\$GOPATH:\$GOPATH/bin:\$PATH
 !
 source ~/.bashrc
 
-echo “======================== Install Drive App =======================”
+echo ======================== Install Drive App =======================
 go get -u github.com/odeke-em/drive/cmd/drive
 
-echo “======================== Test Drive App =======================”
-drive help
+echo ======================== Initialize Drive App =======================
+echo "open the link in a browser then copy and paste the Key"
+drive init
