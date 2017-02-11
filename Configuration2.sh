07728340fdf9d7620276991135ad1c13eca536f6
Geordam <geordam.ly@gmail.com>
2017-02-11 11:28:46 +0000

    Update Configuration2.sh

--- Configuration2.sh
+++ Configuration2.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-echo ======================== Install Drive App =======================
-go get -u github.com/odeke-em/drive/cmd/drive
-
 echo ======================== Enter API Keys =======================
 echo -n "Enter your Hipchat API key Send Notification : "
 read apikeymessage
@@ -25,8 +22,11 @@ fi
 
 echo ======================== Adding API keys in the script file =======================
 sed -i 's/AUTH_TOKEN_NOTIFICATION=/AUTH_TOKEN_NOTIFICATION='$apikeymessage'/g' ~/Code/ScriptCode2.sh
+if [[ $? !=0 ]]; then echo ""; echo "!!!!!! An error occured adding the token for Hipchat notificaton room, please add manually !!!!!!" ; fi
 sed -i 's/AUTH_TOKEN_CREATEROOM=/AUTH_TOKEN_CREATEROOM='$apikeycreateroom'/g' ~/Code/ScriptCode2.sh
+if [[ $? !=0 ]]; then echo ""; echo "!!!!!! An error occured adding the token for Hipchat Manage room, please add manually !!!!!!" ; fi
 sed -i 's/AUTH_TOKEN_BITLY=/AUTH_TOKEN_BITLY='$apikeybitly'/g' ~/Code/ScriptCode2.sh
+if [[ $? !=0 ]]; then echo ""; echo "!!!!!! An error occured adding the token for BitLy !!!!!!" ; fi
 
 echo ======================== Initialize Drive App =======================
 echo "open the link in a browser then Copy and Paste the authorization code:"
