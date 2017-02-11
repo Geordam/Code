a98fb94249d0c43921a4e75d4d77078d8d4073fd
Geordam <geordam.ly@gmail.com>
2017-02-11 12:32:44 +0000

    Update Conf.sh

--- Conf.sh
+++ Conf.sh
@@ -22,11 +22,11 @@ fi
 
 echo ======================== Adding API keys in the script file =======================
 sed -i 's/AUTH_TOKEN_NOTIFICATION=/AUTH_TOKEN_NOTIFICATION='$apikeymessage'/g' ~/Code/ScriptCode.sh
-if [[ $? !=0 ]]; then echo ""; echo "!!!!!! An error occured adding the token for Hipchat notificaton room, please add manually !!!!!!" ; fi
+if [[ $? != 0 ]]; then echo ""; echo "!!!!!! An error occured adding the token for Hipchat notificaton room, please add manually !!!!!!" ; fi
 sed -i 's/AUTH_TOKEN_CREATEROOM=/AUTH_TOKEN_CREATEROOM='$apikeycreateroom'/g' ~/Code/ScriptCode.sh
-if [[ $? !=0 ]]; then echo ""; echo "!!!!!! An error occured adding the token for Hipchat Manage room, please add manually !!!!!!" ; fi
+if [[ $? != 0 ]]; then echo ""; echo "!!!!!! An error occured adding the token for Hipchat Manage room, please add manually !!!!!!" ; fi
 sed -i 's/AUTH_TOKEN_BITLY=/AUTH_TOKEN_BITLY='$apikeybitly'/g' ~/Code/ScriptCode.sh
-if [[ $? !=0 ]]; then echo ""; echo "!!!!!! An error occured adding the token for BitLy !!!!!!" ; fi
+if [[ $? != 0 ]]; then echo ""; echo "!!!!!! An error occured adding the token for BitLy !!!!!!" ; fi
 
 echo ======================== Initialize Drive App =======================
 echo "open the link in a browser then Copy and Paste the authorization code:"
